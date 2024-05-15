166af2b3c75293def3af98b1f8fd27049e3e2c69
QtaZva <qtazva@mail.ru>
2024-05-15 11:51:36 +0900

    Работающая регистрация и вход, без проверки

--- FacultyApp/MainWindow.xaml.cs
+++ FacultyApp/MainWindow.xaml.cs
@@ -1,9 +1,11 @@
-﻿using FacultyApp.Windows;
+﻿using FacultyApp.Classes;
+using FacultyApp.Windows;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.UI.WebControls;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -18,14 +20,37 @@ namespace FacultyApp
 {
     public partial class MainWindow : Window
     {
+        ApplicationContext db;
         public MainWindow()
         {
             InitializeComponent();
+            db = new ApplicationContext();
         }
 
         private void Login_Button_Click(object sender, RoutedEventArgs e)
         {
+            string login = LoginTextBox.Text;
+            string password = PasswordTextBox.Password;
 
+            /*
+            User authUser = new User();
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                authUser = db.Users.Where(b => b.Login == login && b.Pass == pass_1).FirstOrDefault();
+            }
+            */
+
+            Students user = new Students();
+            user = db.Students.Where(u => u.Login == login && u.Password == password).FirstOrDefault();
+
+            if (user != null)
+            {
+                MessageBox.Show("Success");
+            }
+            else
+            {
+                MessageBox.Show("Login or password was entered incorrectly");
+            }
         }
 
         private void CreateAccount_Button_Click(object sender, RoutedEventArgs e)
--- FacultyApp/Windows/RegisterWindow.xaml.cs
+++ FacultyApp/Windows/RegisterWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FacultyApp.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,19 +20,33 @@ namespace FacultyApp.Windows
     /// </summary>
     public partial class RegisterWindow : Window
     {
+        ApplicationContext db;
         public RegisterWindow()
         {
             InitializeComponent();
+            db = new ApplicationContext();
         }
 
         private void Login_Button_Click(object sender, RoutedEventArgs e)
         {
-
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
         }
 
         private void Register_Button_Click(object sender, RoutedEventArgs e)
         {
+            string login = LoginTextBox.Text;
+            string password = PasswordTextBox.Password;
+            string name = NameTextBox.Text;
+            string surname = SurnameTextBox.Text;
+            string patronymic = PatronymicTextBox.Text;
+            string birthdayDate = BirthdayDatePicker.Text;
 
+            Students newstudent = new Students(name, surname, patronymic, birthdayDate, login, password);
+
+            db.Students.Add(newstudent);
+            db.SaveChanges();
         }
     }
 }
